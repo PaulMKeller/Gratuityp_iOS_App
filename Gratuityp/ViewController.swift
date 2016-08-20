b00d44d7c3e77c835ad1a5e8feb27c82da2e8cae
PaulKeller <paul.keller.it@gmail.com>
2016-08-20 21:47:39 +0800

    Initial Project Commit
    Initial Project Commit

---
+++ Gratuityp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gratuityp
+//
+//  Created by Paul Keller on 17/06/2016.
+//  Copyright © 2016 Planet K Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
